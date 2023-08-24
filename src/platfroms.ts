7b001eaa2b0abd5c303e4b6d553a5b3447129d15
Zakary Loney <zakaryloney@gmail.com>
2023-08-24 11:41:45 -0400

    optimized

---
+++ src/platfroms.ts
@@ -0,0 +1,11 @@
+export var platforms = {
+  Bigcommerce: `<meta name='platform' content='bigcommerce.stencil`,
+  Ecwid: `<meta name="generator" content="ec-instant-site" />`,
+  Lightspeed: `<link rel="shortcut icon" href="https://cdn.shoplightspeed.com`,
+  Magento: `<script type="text/x-magento-init">`,
+  Shopify: `<link rel="preconnect" href="https://cdn.shopify.com">`,
+  Square: `<meta name="generator" content="Square Online">`,
+  Squarespace: `<link rel="preconnect" href="https://images.squarespace-cdn.com">`,
+  Wix: `<meta name="generator" content="Wix.com Website Builder"/>`,
+  WooCommerce: `<style id='woocommerce-inline-inline-css' type='text/css'>`,
+};
