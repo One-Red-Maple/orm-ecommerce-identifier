2ff0713d5dc9210e451e888700a49bffd77a55d0
Eric Dion <eric.dion@oneredmaple.com>
2023-12-28 16:13:10 -0500

    Fixed presta shop identifier

--- dist/platforms.js
+++ dist/platforms.js
@@ -12,6 +12,6 @@ exports.platforms = {
     Wix: `meta[content='Wix.com Website Builder'][name='generator']`,
     Shoplazza: `#shoplaza-section-header`,
     Weebly: `link[id='wsite-base-style'][rel='stylesheet'][type='text/css'][href^='//cdn2.editmysite.com']`,
-    PrestaShop: `script:contains('var prestashop')`,
+    PrestaShop: `meta[content='PrestaShop'][name='generator']`,
     WooCommerce: `meta[name='generator'][content*='WooCommerce'], body.woocommerce .woocommerce-Price-amount.amount, style[id='woocommerce-inline-inline-css'][type='text/css']`
 };
