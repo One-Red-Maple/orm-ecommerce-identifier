40166f0203dd8de753fc5b9f5ce712d59392a2db
Eric Dion <94758547+ericdionorm@users.noreply.github.com>
2023-12-28 14:21:18 -0500

    Changed Shoplazza identifier

--- src/platforms.ts
+++ src/platforms.ts
@@ -7,7 +7,7 @@ export var platforms = {
   Square: `meta[content='Square Online'][name='generator']`,
   Squarespace: `link[href^='https://images.squarespace-cdn.com'][rel='preconnect']`,
   Wix: `meta[content='Wix.com Website Builder'][name='generator']`,
-  Shoplazza: `script:contains('app_root_domain: "shoplazza.com"'), img[src^='https://cdn.shoplazza.com/']`,
+  Shoplazza: `#shoplaza-section-header`,
   Weebly: `link[id='wsite-base-style'][rel='stylesheet'][type='text/css'][href^='//cdn2.editmysite.com']`,
   PrestaShop: `script:contains('var prestashop')`,
   WooCommerce: `meta[name='generator'][content*='WooCommerce'], body.woocommerce .woocommerce-Price-amount.amount, style[id='woocommerce-inline-inline-css'][type='text/css']`
